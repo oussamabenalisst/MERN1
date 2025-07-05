cf66daaf265b363149e30f51ecdce898137491ce
oussamabenalisst <benalio248@gmail.com>
2025-07-05 18:44:50 +0100

    add function add

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 export default function Home() {
   type Product = {
@@ -9,6 +9,10 @@ export default function Home() {
     img: string;
   };
   const [products, Setproducts] = useState<Product[]>([]);
+  const nom = useRef<HTMLInputElement>(null);
+  const prix = useRef<HTMLInputElement>(null);
+  const qat = useRef<HTMLInputElement>(null);
+  const img = useRef<HTMLInputElement>(null);
   useEffect(() => {
     const datafetch = async () => {
       try {
@@ -23,9 +27,47 @@ export default function Home() {
     };
     datafetch();
   }, []);
+  const add = async () => {
+    if (nom.current && prix.current && qat.current && img.current) {
+      if (
+        nom.current.value == "" ||
+        prix.current.value == "" ||
+        img.current.value == "" ||
+        qat.current.value == ""
+      ) {
+        alert("vide");
+        return;
+      }
+      const ex = await axios.get("http://localhost:5000/product", {
+        params: {
+          name: nom.current.value,
+        },
+      });
+      if (Array.isArray(ex.data) && !Array.isArray(ex.data)) {
+        alert("The product is available");
+        return;
+      }
+      const prd: Product = {
+        name: nom.current.value,
+        class: qat.current.value,
+        pr: Number(prix.current.value),
+        img: img.current.value,
+      };
+      await axios
+        .post("http://localhost:5000/Addproduct", prd)
+        .then(() => {
+          alert("Gode");
+        })
+        .catch(() => {
+          alert("Error post");
+          return;
+        });
+      Setproducts([...products, prd]);
+    }
+  };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] mt-50">
-      <form className="max-w-md mx-auto p-6 bg-gray-900 border border-gray-700 rounded-lg shadow-md text-gray-200 space-y-4">
+      <div className="max-w-md mx-auto p-6 bg-gray-900 border border-gray-700 rounded-lg shadow-md text-gray-200 space-y-4">
         <h2 className="text-xl font-semibold mb-4 text-center">Add Product</h2>
 
         <div>
@@ -40,6 +82,7 @@ export default function Home() {
             id="name"
             name="name"
             className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600"
+            ref={nom}
           />
         </div>
 
@@ -55,6 +98,7 @@ export default function Home() {
             id="prix"
             name="prix"
             className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600"
+            ref={prix}
           />
         </div>
 
@@ -70,6 +114,7 @@ export default function Home() {
             id="class"
             name="class"
             className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600"
+            ref={qat}
           />
         </div>
         <div>
@@ -84,16 +129,18 @@ export default function Home() {
             id="IMG"
             name="IMG"
             className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600"
+            ref={img}
           />
         </div>
 
         <button
           type="submit"
           className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md transition duration-200"
+          onClick={add}
         >
           💾 save
         </button>
-      </form>
+      </div>
 
       <table className="min-w-full text-sm rounded-md overflow-hidden shadow-lg border border-gray-700 bg-gray-900 text-gray-200 dark:bg-gray-800 dark:text-gray-200">
         <thead className="bg-gray-800 dark:bg-gray-700">
@@ -112,7 +159,7 @@ export default function Home() {
         <tbody>
           {products.map((product, index) => (
             <tr
-              key={index}
+              key={product.name}
               className={`${
                 index % 2 === 0 ? "bg-gray-800" : "bg-gray-900"
               } hover:bg-gray-700 transition duration-200`}
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors";
 const Product = require("./src/models/products");
 const app = express();
@@ -19,6 +19,46 @@ app.use(
     credentials: true,
   })
 );
+app.delete("/product/:name", async (req: Request, res: Response) => {
+  if (!req.params.name) {
+    res.status(403).send("name vide");
+    return;
+  }
+  const name = req.params.name;
+  if (name == "" || name.length < 3 || name.length > 50) {
+    res.status(403).send("name not valide");
+    return;
+  }
+  await Product.deleteMany({ name: name })
+    .then(() => {
+      res.status(200).send("Good delete");
+    })
+    .catch(() => {
+      res.status(403).send("error delete");
+    });
+});
+app.post("/Addproduct", (req: Request, res: Response) => {
+  try {
+    if (!req.body) {
+      res.send("error req.body");
+    }
+    var Newproduct = new Product();
+    const { name: nameP, class: ClassP, pr, img } = req.body;
+    if (!nameP || !ClassP || !pr || !img) {
+      res.send("error input");
+      return;
+    }
+    Newproduct.name = nameP;
+    Newproduct.class = ClassP;
+    Newproduct.pr = pr;
+    Newproduct.img = img;
+    Newproduct.save().then(() => {
+      res.send("product add");
+    });
+  } catch {
+    res.send("Error of catch");
+  }
+});
 
 app.get("/product", async (req: Request, res: Response) => {
   try {
@@ -26,6 +66,21 @@ app.get("/product", async (req: Request, res: Response) => {
     res.json(prd);
   } catch (err) {
     res.send(err);
+  }
+});
+app.get("/product/:name", async (req: Request, res: Response) => {
+  if (req.params) {
+    try {
+      const name = req.params.name;
+      if (name == "" || name.length < 3 || name.length > 50) {
+        res.status(403).send("name not valide");
+        return;
+      }
+      const prd = await Product.find({ name: name });
+      res.json(prd);
+    } catch {
+      res.send("Error");
+    }
   }
 });
 
