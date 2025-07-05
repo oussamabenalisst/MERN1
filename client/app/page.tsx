bf90ec4661f9ea12869d13e9ad6975819a864786
oussamabenalisst <benalio248@gmail.com>
2025-07-05 14:03:49 +0100

    update proxy to port 5000

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 import { useEffect, useState } from "react";
-
 export default function Home() {
-  const [Base, SetDB] = useState([{}]);
+  const [products, Setproducts] = useState([{}]);
   useEffect(() => {
-    fetch("/product")
-      .then((Response) => Response.json())
-      .then((data) => {
-        SetDB(data);
-      });
+    const datafetch = async () => {
+      await fetch("/product")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("test");
+          console.log(data);
+          Setproducts(data);
+        });
+    };
+    datafetch();
   }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      {products.map((product, index) => {
+        return <p key={index}>{product.name}</p>;
+      })}
     </div>
   );
 }
